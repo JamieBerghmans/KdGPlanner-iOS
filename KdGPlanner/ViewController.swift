a50438d062bd1f34bf6ec368ddcdf00a7c7d158d
Jamie Berghmans <jamie.berghmans@student.kdg.be>
2019-09-11 15:55:46 +0200

    Initial Commit

---
+++ KdGPlanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KdGPlanner
+//
+//  Created by Jamie Berghmans on 11/09/2019.
+//  Copyright © 2019 Devvix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
