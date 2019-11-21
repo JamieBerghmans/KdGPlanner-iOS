6667ca8e459dbf75e74bfafdf676b71af296c602
Jamie Berghmans <jamie.berghmans@student.kdg.be>
2019-11-21 17:35:16 +0100

    Watchos functionality

---
+++ Watch Extension/ClassroomRowController.swift
@@ -0,0 +1,11 @@
+//
+//  ClassroomRow.swift
+//  Watch Extension
+//
+//  Created by Jamie Berghmans on 15/11/2019.
+//  Copyright © 2019 Devvix. All rights reserved.
+//
+
+import WatchKit
+
+class ClassroomRow: WKInterfaceObject
